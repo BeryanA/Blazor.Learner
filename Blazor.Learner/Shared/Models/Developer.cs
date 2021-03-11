a31cdc6993c4eb60717884471b2e452001b0a9ae
BAS <asberyan@gmail.com>
2021-03-11 15:49:29 +0400

    Add project files.

---
+++ Blazor.Learner/Shared/Models/Developer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Blazor.Learner.Shared.Models
+{
+    public class Developer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Experience { get; set; }
+    }
+}
